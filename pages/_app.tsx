1b3be01efeb4d4afba21311cb0cfd105a03758e2
Ronni Egeriis Persson <rpersson@seatgeek.com>
2021-09-16 14:28:19 -0400

    Install polaris and add alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import '@p/dist/styles.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import enTranslations from '@p/locales/en.json';
+import { AppProvider } from '@p';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Card, Button } from '@p';
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,10 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Card sectioned>
+          <Button onClick={() => alert('Button clicked!')}>Example button</Button>
+        </Card>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
